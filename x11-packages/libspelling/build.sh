81b0be04d4592307e73c00d544b7e70705449814
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-11-10 14:24:50 +0800

    libicu: revbump revdeps (4)
    packages: libgnustep-base, harfbuzz, postgresql, php
    x11-packages: libspelling, libgedit-tepl

--- x11-packages/libspelling/build.sh
+++ x11-packages/libspelling/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Spellcheck library for GTK 4"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.4.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libspelling/${TERMUX_PKG_VERSION%.*}/libspelling-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ac9132c634bb9df911e35b52345aa12126f7ec95e5f07179a980861819787150
 TERMUX_PKG_DEPENDS="enchant, glib, gtk4, gtksourceview5, libicu, pango"
